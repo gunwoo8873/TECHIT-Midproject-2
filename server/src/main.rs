599c0e79f48b6a5724c3999deb316585e18fc961
choigunwoo <gunwoo8873@outlook.kr>
2024-09-13 10:11:40 +0900

    Update Web Server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     fs, thread, time::Duration,
+    hello::ThreadPool,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -9,7 +10,13 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -22,13 +29,24 @@ fn handle_connection(mut stream: TcpStream) {
     let index_path = "../public/index.html";
     let notfound_path = "../public/404.html";
 
-    //// HTTP Web Response Status Check
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", index_path)
-    }
-    else {
-        ("HTTP/1.1 404 NOT FOUND", notfound_path)
+    //// HTTP Web Response Status Check [Single Thread]
+    // let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    //     ("HTTP/1.1 200 OK", index_path)
+    // }
+    // else {
+    //     ("HTTP/1.1 404 NOT FOUND", notfound_path)
+    // };
+
+    //// HTTP Web Response Status Check [Multi Thread]
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", index_path),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", index_path)
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", notfound_path),
     };
+
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
